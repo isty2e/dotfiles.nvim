bc55ce6192ab5e3cb4837233e74318c0a8d9e435
isty2e <soinbae@gmail.com>
2024-08-12 23:38:59 +0900

    feat(ui): add mini-animate

--- lua/plugins/astrocommunity.lua
+++ lua/plugins/astrocommunity.lua
@@ -3,5 +3,6 @@ return {
   { import = "astrocommunity.colorscheme.github-nvim-theme" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.python-ruff" },
-  { import = "astrocommunity.scrolling.satellite-nvim" }
+  { import = "astrocommunity.scrolling.satellite-nvim" },
+  { import = "astrocommunity.scrolling.mini-animate" },
 }
