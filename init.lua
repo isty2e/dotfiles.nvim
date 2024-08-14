ab07875d05f5075b7f13906fbabb0779e2f96846
hyunjunji <hyunjun.ji@standigm.com>
2024-08-14 12:43:14 +0900

    fix: temporary fix to make vmode keys functioning

--- init.lua
+++ init.lua
@@ -24,3 +24,54 @@ require "polish"
 vim.cmd("set nornu")
 vim.cmd("colorscheme github_light")
 vim.cmd("set wrap")
+
+-- Colemak mapping
+vim.g.colemak_mapping = 1
+
+local function colemak_to_qwerty()
+  local mappings = {
+    { '', 'f', 'e' }, { 'n', 'p', 'r' }, { 'x', 'p', 'r' }, { 'o', 'p', 'r' },
+    { '', 'g', 't' }, { '', 'j', 'y' }, { '', 'l', 'u' }, { '', 'u', 'i' },
+    { '', 'y', 'o' }, { '', ';', 'p' }, { '', 'r', 's' }, { '', 's', 'd' },
+    { '', 't', 'f' }, { '', 'd', 'g' }, { '', 'n', 'j' }, { '', 'e', 'k' },
+    { '', 'i', 'l' }, { '', 'o', ':' }, { '', 'k', 'n' },
+    -- Shift combinations
+    { '', 'F', 'E' }, { 'n', 'P', 'R' }, { 'x', 'P', 'R' }, { 'o', 'P', 'R' },
+    { '', 'G', 'T' }, { '', 'J', 'Y' }, { '', 'L', 'U' }, { '', 'U', 'I' },
+    { '', 'Y', 'O' }, { '', ':', 'P' }, { '', 'R', 'S' }, { '', 'S', 'D' },
+    { '', 'T', 'F' }, { '', 'D', 'G' }, { '', 'N', 'J' }, { '', 'E', 'K' },
+    { '', 'I', 'L' }, { '', 'O', ':' }, { '', 'K', 'N' },
+    -- Ctrl combinations
+    { '', '<C-s>', '<C-d>' }, { '', '<C-l>', '<C-u>' },
+    -- Additional mappings
+    { '', 'jj',    'yy' }, { '', 'dd', 'gg' }, { '', 'ss', 'dd' }
+  }
+
+  for _, m in ipairs(mappings) do
+    local mode, from, to = unpack(m)
+    vim.api.nvim_set_keymap(mode, from, to, { noremap = true })
+  end
+end
+
+local function restore_qwerty()
+  vim.cmd('mapclear')
+  vim.cmd('mapclear!')
+end
+
+function _G.toggle_colemak_mapping()
+  if vim.g.colemak_mapping == 1 then
+    restore_qwerty()
+    vim.g.colemak_mapping = 0
+    print("QWERTY mapping activated")
+  else
+    colemak_to_qwerty()
+    vim.g.colemak_mapping = 1
+    print("Colemak mapping activated")
+  end
+end
+
+-- Activate Colemak mapping on startup
+colemak_to_qwerty()
+
+-- Set toggle shortcut
+vim.api.nvim_set_keymap('n', '<leader>cm', ':lua toggle_colemak_mapping()<CR>', { noremap = true, silent = true })
